469cf2540fb1e670f3cf92f955160247144eec2f
Andrzej Wasiak <a.wasiak@sarehub.com>
2016-07-15 15:46:02 +0200

    added HttpClientMock

---
+++ src/SAREhub/Commons/Http/HttpClientMockBuilder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace SAREhub\Commons\Http;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+
+class HttpClientMockBuilder {
+	
+	/** @var array */
+	private $responseQueue = [];
+	
+	/**
+	 * @param Response $response
+	 * @return $this
+	 */
+	public function response(Response $response) {
+		$this->responseQueue[] = $response;
+		return $this;
+	}
+	
+	/**
+	 * @param RequestException $exception
+	 * @return $this
+	 */
+	public function requestException(RequestException $exception) {
+		$this->responseQueue[] = $exception;
+		return $this;
+	}
+	
+	/**
+	 * @return array
+	 */
+	public function getResponseQueue() {
+		return $this->responseQueue;
+	}
+	
+	/**
+	 * @return Client
+	 */
+	public function build() {
+		return new Client([
+		  'handler' => HandlerStack::create(new MockHandler($this->responseQueue))
+		]);
+	}
+}
---
+++ tests/SAREhub/Commons/Http/HttpClientMockBuilderTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace SAREhub\Commons\Http;
+
+
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+
+use PHPUnit\Framework\TestCase;
+
+class HttpClientMockBuilderTest extends TestCase {
+	
+	public function testAddResponse() {
+		$response = new Response(200, [], "TEST");
+		$builder = (new HttpClientMockBuilder())->response($response);
+		$this->assertContains($response, $builder->getResponseQueue());
+	}
+	
+	public function testAddRequestException() {
+		$requestException = new RequestException("test", new Request("GET", "http://example.com/"));
+		$builder = (new HttpClientMockBuilder())->requestException($requestException);
+		$this->assertContains($requestException, $builder->getResponseQueue());
+	}
+	
+	public function testSimpleResponse() {
+		$realResponse = new Response(200, [], "TEST");
+		$httpClient = (new HttpClientMockBuilder())->response($realResponse)->build();
+		$response = $httpClient->get("/test");
+		$this->assertEquals($realResponse, $response);
+	}
+	
+	
+}
