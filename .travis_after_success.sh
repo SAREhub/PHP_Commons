ea419ed7b1e9fbc8540b3abfd5787b139cde401c
Andrzej Wasiak <a.wasiak@sarehub.com>
2016-07-22 10:20:12 +0200

    added phpdoc generate and upload to gh_pages

---
+++ .travis_after_success.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "SAREhub/PHP_Commons" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PHP_VERSION" == "5.6" ]; then
+
+  echo -e "Publishing PHPDoc...\n"
+  cp -R build/docs $HOME/docs-latest
+
+  cd $HOME
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/<Votre depot> gh-pages > /dev/null
+
+  cd gh-pages
+
+  git rm -rf ./docs/$TRAVIS_BRANCH
+
+  mkdir docs
+  cd docs
+  mkdir $TRAVIS_BRANCH
+
+  cp -Rf $HOME/docs-latest/* ./$TRAVIS_BRANCH/
+
+  git add -f .
+  git commit -m "PHPDocumentor (Travis Build : $TRAVIS_BUILD_NUMBER  - Branch : $TRAVIS_BRANCH)"
+  git push -fq origin gh-pages > /dev/null
+  echo -e "Published PHPDoc to gh-pages.\n"
+fi
