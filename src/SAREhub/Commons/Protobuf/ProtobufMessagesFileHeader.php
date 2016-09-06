bf405bd4a430c91194654bfbc33f1d8635ea821d
Andrzej Wasiak <a.wasiak@sarehub.com>
2016-09-06 09:49:26 +0200

    refactored

--- src/SAREhub/Commons/Protobuf/ProtobufMessagesFileHeader.php
+++ src/SAREhub/Commons/Protobuf/ProtobufMessagesFileHeader.php
@@ -6,10 +6,12 @@ use Protobuf\Stream;
 
 /**
  * Represents File header for Protocol Buffers messages file
- * HEADER_FORMAT:
- * 4B            |4B                   |1B                     |
- * --------------|---------------------|-----------------------|
- * HEADER_VERSION| HEADER_CONTENTS_SIZE|MESSAGE_SIZE_INFO_BYTES|
+ *
+ * FORMAT
+ * |------------------------------------------------------------|
+ * |4B            |4B                   |1B                     |
+ * |--------------|---------------------|-----------------------|
+ * |HEADER_VERSION| HEADER_CONTENTS_SIZE|MESSAGE_SIZE_INFO_BYTES|
  */
 class ProtobufMessagesFileHeader {
 	
@@ -22,41 +24,58 @@ class ProtobufMessagesFileHeader {
 	/** @var int */
 	private $messageSizeInfoBytes = 2;
 	
+	public function headerFormatVersion($version) {
+		$this->headerFormatVersion = $version;
+		return $this;
+	}
+	
 	public function messageSizeInfoBytes($bytes) {
 		$this->messageSizeInfoBytes = (int)$bytes;
 		return $this;
 	}
 	
-	public function toBinaryString() {
-		$contents = pack('C', $this->messageSizeInfoBytes);
-		return pack('N', $this->headerFormatVersion).pack('N', strlen($contents)).$contents;
-	}
-	
+	/**
+	 * @param \SplFileObject $file
+	 * @return ProtobufMessagesFileHeader
+	 */
 	public static function readFromFile(\SplFileObject $file) {
 		$file->fread(self::HEADER_CONTENTS_SIZE_INFO_BYTES);
-		$headerFormatVersion = unpack('N', $file->fread(4))[1];
-		$headerContentsSize = unpack('N', $file->fread(4))[1];
+		$headerFormatVersion = unpack('N', $file->fread(self::HEADER_FORMAT_VERSION_BYTES))[1];
+		$headerContentsSize = unpack('N', $file->fread(self::HEADER_CONTENTS_SIZE_INFO_BYTES))[1];
 		return self::fromBinaryString($headerFormatVersion, $file->fread($headerContentsSize));
 	}
 	
+	/**
+	 * @param int $headerFormatVersion
+	 * @param string $contents
+	 * @return ProtobufMessagesFileHeader
+	 */
 	public static function fromBinaryString($headerFormatVersion, $contents) {
 		$header = new self();
-		$header->headerFormatVersion = $headerFormatVersion;
-		
+		$header->headerFormatVersion($headerFormatVersion);
 		$stream = Stream::fromString($contents);
 		$header->messageSizeInfoBytes = unpack('C', $stream->read(1))[1];
 		$stream = null; // for close stream
 		return $header;
 	}
 	
+	/**
+	 * @return int
+	 */
 	public function getHeaderFormatVersion() {
 		return $this->headerFormatVersion;
 	}
 	
+	/**
+	 * @return int
+	 */
 	public function getMessageSizeInfoBytes() {
 		return $this->messageSizeInfoBytes;
 	}
 	
+	/**
+	 * @return string
+	 */
 	public function getMessageSizeInfoPackFormat() {
 		switch ($this->messageSizeInfoBytes) {
 			case 2:
@@ -66,6 +85,14 @@ class ProtobufMessagesFileHeader {
 			case 8:
 				return 'J';
 		}
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function toBinaryString() {
+		$contents = pack('C', $this->messageSizeInfoBytes);
+		return pack('N', $this->headerFormatVersion).pack('N', strlen($contents)).$contents;
 	}
 	
 }
