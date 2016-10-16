195f023016e369da0bff5fa54b8def0c3b165309
Mararok <mararok@gmail.com>
2016-10-16 15:59:19 +0200

    refactored

--- examples/ExampleCommons.php
+++ examples/ExampleCommons.php
@@ -9,3 +9,10 @@ function logMessage($message) {
 function runProcess($file, &$pipes) {
 	return proc_open('php ' . $file, [1 => ["pipe", "w"]], $pipes, __DIR__);
 }
+
+function printOutputFromProcess($label, $pipe) {
+	logMessage("output from $label: ");
+	echo "--------------------------------\n";
+	echo stream_get_contents($pipe);
+	echo "--------------------------------\n";
+}
--- examples/zmq/publish_subscribe/1/example.php
+++ examples/zmq/publish_subscribe/1/example.php
@@ -5,8 +5,9 @@ use SAREhub\Commons\Zmq\PublishSubscribe\Publisher;
 
 echo "zmq.publish_subscribe example 1 \n";
 
-$publisher = Publisher::inContext(new ZMQContext())->bind(Dsn::tcp()->endpoint('127.0.0.1:10000'));
-logMessage("Connected");
+$publisher = Publisher::inContext(new ZMQContext())
+	->bind(Dsn::tcp()->endpoint('127.0.0.1:10000'));
+logMessage("created and binded");
 logMessage("start subscriber");
 
 $p = runProcess(__DIR__ . '/subscriber.php', $pipes);
@@ -14,14 +15,10 @@ $p = runProcess(__DIR__ . '/subscriber.php', $pipes);
 logMessage("started subscriber");
 sleep(2);
 
-logMessage("Publishing message ...");
+logMessage("publishing message ...");
 $publisher->publish("topic", "message", true);
-sleep(5);
+sleep(2);
 $publisher->unbind();
 logMessage("unbinded");
-
-echo "\nOutput from subscriber: \n";
-echo "\n--------------------------------\n";
-echo stream_get_contents($pipes[1]);
-echo "\n--------------------------------\n";
+printOutputFromProcess("subscriber", $pipes[1]);
 
