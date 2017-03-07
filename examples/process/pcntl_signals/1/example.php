7b82afc1d97abf964e4b0b59a050ffc767e699e1
Andrzej Wasiak <mararok@gmail.com>
2017-03-07 14:34:56 +0100

    revisited pcntl signal for better support mocking (#26)
    * revisited pcntl signal

--- examples/process/pcntl_signals/1/example.php
+++ examples/process/pcntl_signals/1/example.php
@@ -5,12 +5,12 @@ use SAREhub\Commons\Process\PcntlSignals;
 echo "PcntlSignals example\n\n";
 echo "press CTRL+C to call SIGINT signal handler \n";
 
-PcntlSignals::getGlobal()->handle(\SIGINT, function () {
+$signals = PcntlSignals::create()->handle(\SIGINT, function () {
 	echo "\nkilled via signal\n";
 	exit(0);
-})->install();
+});
 
 while (true) {
-	PcntlSignals::getGlobal()->checkPendingSignals();
+	$signals->checkPendingSignals();
 };
 
