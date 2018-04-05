0f6f2f3f34571e3b2397b46b3fe47732618b3795
Andrzej Wasiak <mararok@gmail.com>
2018-04-05 15:47:55 +0200

    removed constructor from exception (#36)

--- src/SAREhub/Commons/Secret/SecretValueNotFoundException.php
+++ src/SAREhub/Commons/Secret/SecretValueNotFoundException.php
@@ -6,16 +6,5 @@ namespace SAREhub\Commons\Secret;
 
 class SecretValueNotFoundException extends \Exception
 {
-    private $secretName;
-
-    /**
-     *
-     * @param string $secretName
-     */
-    public function __construct(string $secretName)
-    {
-        $this->secretName = $secretName;
-    }
-
 
 }
